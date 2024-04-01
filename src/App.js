d04de9226d5918a9c9999bec87b1f628aa171089
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-01 20:59:01 +0300

    View Basic Animation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import BasicsOfMotion from "./components/BasicsOfMotion";
 import Gestures from "./components/Gestures";
 import AnimationControl from "./components/AnimationControl";
+import ViewBasicAnimations from "./components/ViewBasicAnimations";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
           <Route path="/basics-motion" element={<BasicsOfMotion/>}/>
           <Route path="/gestures" element={<Gestures/>}/>
           <Route path="/animal-control" element={<AnimationControl/>}/>
+          <Route path="/view-basic-animation" element={<ViewBasicAnimations/>}/>
       </Routes>
     </div>
   );
