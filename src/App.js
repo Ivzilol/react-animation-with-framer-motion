017e0b85343ef2e6bf9b328e581ad65c5b21ad13
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-01 20:05:49 +0300

    Gestures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import BasicsOfMotion from "./components/BasicsOfMotion";
+import Gestures from "./components/Gestures";
 function App() {
   return (
     <div className="App">
       <Routes>
           <Route path="/basics-motion" element={<BasicsOfMotion/>}/>
+          <Route path="/gestures" element={<Gestures/>}/>
       </Routes>
     </div>
   );
--- src/components/BasicsOfMotion.js
+++ src/components/BasicsOfMotion.js
@@ -26,19 +26,23 @@ const BasicsOfMotion = () => {
                 {isVisible && (<motion.div
                     initial={{
                         rotate: '0deg',
-                        scale: 0
+                        scale: 0,
+                        y: 0
                     }}
                     animate={{
                         rotate: '180deg ',
-                        scale: 1
+                        scale: 1,
+                        y: [0, 150, -150, -150, 0]
                     }}
                     exit={{
                         rotate: '0deg',
-                        scale: 0
+                        scale: 0,
+                        y: 0
                     }}
                     transition={{
                         duration: 1,
-                        ease: 'backInOut'
+                        ease: 'backInOut',
+                        times: [0, 0.25, 0.5, 0.85, 1]
                     }}
                     style={{
                         width: 150,
